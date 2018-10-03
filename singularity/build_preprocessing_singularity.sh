76095af3cb50a2c600942c4fdd3443287218658f
Jessica Dafflon <jejedafflon@gmail.com>
2018-10-03 16:25:08 +0000

    Working! Run bet on container
    * Use neurodocker to create a singularity image with fsl and necessary python
     packages to run bet on one sample subject
    
    Detailed description:
    * build_preprocessing_singularity.sh: Create a new singularity image from the
    automatically generated neurodocker singularity configuration file
    * rename file to run the preprocessingsingularity container
    * preprocessingSingularity: Add neurodocker configuration file

---
+++ singularity/build_preprocessing_singularity.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# If it exists remove old image
+if [ -f preprocessing_fsl.img ]; then
+    rm preprocessingSingularity preprocessing_fsl.img
+    echo 'Remove old files'
+fi
+
+mv preprocessing5 preprocessingSingularity
+echo 'rename preprocessing5 into preprocessingSingularity'
+
+singularity image.create preprocessing_fsl.img
+echo 'created singularity image'
+
+sudo singularity build preprocessing_fsl.img preprocessingSingularity
+echo 'Linked image to recipe'
