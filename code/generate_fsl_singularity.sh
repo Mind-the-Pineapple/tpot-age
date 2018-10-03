5752e2770dceb947d3149700d0602ef4d1398d14
Jessica Dafflon <jejedafflon@gmail.com>
2018-10-03 15:06:08 +0100

    Use previous and stable neurodocker version
    * Use neurodocker v.0.4.1 and not master. This avoid problems with the different  python packages

--- code/generate_fsl_singularity.sh
+++ code/generate_fsl_singularity.sh
@@ -4,15 +4,15 @@ set -e
 
 # Singularity recipe.
 generate() {
-    docker run --rm kaczmarj/neurodocker:master generate "$1" \
+    docker run --rm kaczmarj/neurodocker:0.4.1 generate "$1" \
         --base=neurodebian:stretch-non-free \
         --pkg-manager=apt \
         --install fsl vim git coreutils\
         --add-to-entrypoint='source /etc/fsl/fsl.sh' \
         --miniconda create_env=neuro \
-                    conda_install='python=3.6 numpy=1.15.2 pandas=0.23.4' \
-                                  'nipype=1.1.3 scipy=1.1.0 scikit-learn=0.19.2'\
-                    activate=true \
+                    conda_install='python=3.6 numpy=1.15.2 pandas=0.23.4 nipype=1.1.3
+                                 scipy=1.1.0 scikit-learn=0.19.2 ipython=6.5.0' \
+                    activate=True \
         --user=neuro \
         --workdir='/home/neuro'
 }
