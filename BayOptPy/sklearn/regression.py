0dcb0444781b6b8993adee1b9d53fecc311cd0d5
Jessica Dafflon <jejedafflon@gmail.com>
2018-10-05 21:17:10 +0100

    Perform regression and preprocessing in one script
    * Use nilearn mask function (masking.compute_multi_epi_mask(imgs, lower_cutoff=0.001, upper_cutoff=.7, opening=1)) to find brain voxels
    * Separate training and test data and perform simple linear regression on the
    * data

--- BayOptPy/sklearn/regression.py
+++ BayOptPy/sklearn/regression.py
@@ -3,6 +3,7 @@
 import os
 import re
 import pandas as pd
+from nilearn import masking
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import model_selection
@@ -10,41 +11,8 @@ from sklearn.linear_model import LinearRegression
 import nibabel as nib
 
 
-def data_preprocessing():
-    cwd = os.getcwd()
-    dataPath = os.path.join(cwd, 'data')
-    # remove the file end and get list of all used subjects
-    fileList = os.listdir(dataPath)
-    rawsubjectsId = [re.sub(r'^smwc1(.*?)\_mpr-1_anon.nii$', '\\1', file) for file in fileList if file.endswith('.nii')]
-    # TODO: Change this. For testing purpose select just the first 5 subjects
-    rawsubjectsId = rawsubjectsId[:5]
-
-    # Load image proxies
-    imgs = [nib.load(os.path.join(dataPath, 'smwc1%s_mpr-1_anon.nii' %subject)) for subject in rawsubjectsId]
-    # Load data as numpy array (might get very slow depending on the number of subjects) and reshape the image so that
-    # you have one 1D array with the data.
-    # Note: np.flatten uses c style ('row-wise') to flatten the array.
-    # For example: myarray = np.arange(18).reshape((2,3,3))
-    # array([[[ 0,  1,  2],
-    #         [ 3,  4,  5],
-    #         [ 6,  7,  8]],
-    #        [[ 9, 10, 11],
-    #         [12, 13, 14],
-    #         [15, 16, 17]]])
-    # myarray.flatten()
-    # array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
-    #        17])
-
-    # Create mean image to generate mask
-    imgsData = [subjectImg.get_fdata().flatten() for subjectImg in imgs]
-    meanImg = np.mean(imgsData, axis=0)
-    # reshape data to the correct shape and save image as nii file
-    meanImg = meanImg.reshape(imgs[0].get_fdata().shape)
-    niiMeanImg = nib.Nifti1Image(meanImg, imgs[0].affine)
-    nib.save(niiMeanImg, os.path.join('data','mean_img.nii'))
-
-
-    # Load demographic details
+def get_data_covariates(dataPath, rawsubjectsId):
+    # Load the demographic details from the dataset
     demographics = pd.read_csv(os.path.join(dataPath, 'oasis_cross-sectional.csv'))
     # sort demographics by ascending id
     demographics = demographics.sort_values('ID')
@@ -54,24 +22,56 @@ def data_preprocessing():
     # remove the demographic data from the missing subjects
     demographics = demographics.loc[~demographics['ID'].isin(missingsubjectsId)]
 
+    return demographics
+
+
+if __name__ == '__main__':
+
+    cwd = os.getcwd()
+    print('Current working directory is: %s' %cwd)
+
+    # Get nii images and list of subjects
+    dataPath = os.path.join(cwd, 'data')
+    # remove the file end and get list of all used subjects
+    fileList = os.listdir(dataPath)
+    rawsubjectsId = [re.sub(r'^smwc1(.*?)\_mpr-1_anon.nii$', '\\1', file) for file in fileList if file.endswith('.nii')]
+    # TODO: Change this. For testing purpose select just the first 5 subjects
+    rawsubjectsId = rawsubjectsId[:250]
+
+    # Load the demographics for each subject
+    demographics = get_data_covariates(dataPath, rawsubjectsId)
+
+    # Load image proxies
+    imgs = [nib.load(os.path.join(dataPath, 'smwc1%s_mpr-1_anon.nii' %subject)) for subject in rawsubjectsId]
+
+    # Use nilearn to mask only the brain voxels across subjects
+    MeanImgMask = masking.compute_multi_epi_mask(imgs, lower_cutoff=0.001, upper_cutoff=.7, opening=1)
+    # Apply the group mask on all subjects.
+    # Note: The apply_mask function returns the flattened data as a numpy array
+    maskedData = [masking.apply_mask(img, MeanImgMask) for img in imgs]
+    print('Applied mask to the dataset')
+    # Transform the imaging data into a dataframe (subjects x voxels)
+    imgDf = pd.DataFrame(data=maskedData)
+
     # split train-test dataset
     targetAttribute = demographics['Age']
-    Xtrain, Xtest, Ytrain, Ytest = model_selection.train_test_split(demographics, targetAttribute, test_size=.4, random_state=42)
+    Xtrain, Xtest, Ytrain, Ytest = model_selection.train_test_split(imgDf, targetAttribute, test_size=.4, random_state=42)
+    print('Divided dataset into test and training')
     print('Check train test split sizes')
     print('X_train: ' + str(Xtrain.shape))
     print('X_test: '  + str(Xtest.shape))
     print('Y_train: ' + str(Ytrain.shape))
     print('Y_test: '  + str(Ytest.shape))
-    return Xtrain, Xtest, Ytrain, Ytest, demographics
-
-
-if __name__ == '__main__':
-    # Perform preprocessing
-    Xtrain, Xtest, Ytrain, Ytest, demographics = data_preprocessing()
 
     # Do simple Linear regression
     linReg = LinearRegression()
     linReg.fit(Xtrain, Ytrain)
+    print('Performed simple linear regresssion')
     Ypred = linReg.predict(Xtest)
     # plot the results
     plt.scatter(Ytest, Ypred)
+    plt.xlabel("Age: $Y_i$")
+    plt.ylabel("Predicted Age: $\hat{Y}_i$")
+    plt.title("Age vs Predicted Age: $Y_i$ vs $\hat{Y}_i$")
+    plt.show()
+    print('Done')
