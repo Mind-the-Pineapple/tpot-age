9c0ac3dafa93581792f32392530003d192f3d9b4
Jessica Dafflon <jejedafflon@gmail.com>
2018-10-05 20:57:42 +0100

    Explore nilearn masking functions
    * Try results with the `masking.compute_multibackground_mask()`
    and  `masking.compute_multi_epi_mask()`
    * `masking.compute_multi_epi_mask() allows for setting a threshold for the mask`

--- BayOptPy/preprocessing/preprocessing.py
+++ BayOptPy/preprocessing/preprocessing.py
@@ -5,15 +5,14 @@ import re
 
 import numpy as np
 import nibabel as nib
+from nilearn import masking
 from nipype.interfaces.fsl import BET
 from nipype import SelectFiles, Node
 from nipype.pipeline.engine import Workflow
 from nipype.interfaces.io import DataSink, DataGrabber
 
 
-def get_mean_image(dataPath, rawsubjectsId):
-    # Load image proxies
-    imgs = [nib.load(os.path.join(dataPath, 'smwc1%s_mpr-1_anon.nii' %subject)) for subject in rawsubjectsId]
+def get_mean_image(dataPath, rawsubjectsId, imgs):
     # Load data as numpy array (might get very slow depending on the number of subjects) and reshape the image so that
     # you have one 1D array with the data.
     # Note: np.flatten uses c style ('row-wise') to flatten the array.
@@ -34,7 +33,11 @@ def get_mean_image(dataPath, rawsubjectsId):
     # reshape data to the correct shape and save image as nii file
     meanImg = meanImg.reshape(imgs[0].get_fdata().shape)
     niiMeanImg = nib.Nifti1Image(meanImg, imgs[0].affine)
-    nib.save(niiMeanImg, os.path.join(dataPath, 'mean_img.nii'))
+    meanImgPath = os.path.join(dataPath, 'mean_img')
+    if not os.path.isdir(meanImgPath):
+        os.mkdir(meanImgPath)
+    nib.save(niiMeanImg, os.path.join(meanImgPath, 'mean_img.nii'))
+    return niiMeanImg
 
 def bet_nipype_pipeline(dataPath):
     # Get image
@@ -42,7 +45,7 @@ def bet_nipype_pipeline(dataPath):
     dataSource = Node(interface=DataGrabber(outfields=['t1']),
                                             name='DataSource')
     dataSource.inputs.base_directory = os.getcwd()
-    dataSource.inputs.template = 'data/mean_img.nii'
+    dataSource.inputs.template = 'data/mean_img/mean_img.nii'
     dataSource.inputs.sort_filelist = True
 
     bet = Node(BET(), name='bet')
@@ -86,9 +89,27 @@ if __name__ == '__main__':
     # TODO: Change this. For testing purpose select just the first 5 subjects
     rawsubjectsId = rawsubjectsId[:5]
 
+    # Load image proxies
+    imgs = [nib.load(os.path.join(dataPath, 'smwc1%s_mpr-1_anon.nii' %subject)) for subject in rawsubjectsId]
+
     # get main image from the dataset
-    get_mean_image(dataPath, rawsubjectsId)
+    #niiMeanImg = get_mean_image(dataPath, rawsubjectsId, imgs)
+
+    # clever way of getting the mean image with a single funciton in nilearn
+    # As with the other way, this is still bigger the the brain, but will use this for now
+    niiMeanImg = masking.compute_multi_background_mask(imgs)
+    # Apply the group mask on all subjects. The apply mask return the flattened data as a numpy array
+    maskedData = [masking.apply_mask(img, niiMeanImg) for img in imgs]
+
+    #try to use epi mask
+    niiMeanImgEpi = masking.compute_multi_epi_mask(imgs)
 
     # get brain mask for the mean image
-    bet_nipype_pipeline(dataPath)
+    #bet_nipype_pipeline(dataPath)
+
+
+    maskedData = [masking.apply_mask(img, niiMeanImg) for img in imgs]
+    # to bring the data back to a 3D shape
+    # unmaskedData = masking.unmask(maskedData, niiMeanImg)
+
 
