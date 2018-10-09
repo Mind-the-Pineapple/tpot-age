af69964c49bf8d11b045fea1c78052f30004d5cd
Jessica Dafflon <jejedafflon@gmail.com>
2018-10-09 13:55:20 +0100

    Create helperfunction to get the brain data

---
+++ BayOptPy/helperfunctions.py
@@ -0,0 +1,48 @@
+import os
+import re
+import pandas as pd
+from nilearn import masking
+import nibabel as nib
+
+
+def get_data_covariates(dataPath, rawsubjectsId):
+    # Load the demographic details from the dataset
+    demographics = pd.read_csv(os.path.join(dataPath, 'oasis_cross-sectional.csv'))
+    # sort demographics by ascending id
+    demographics = demographics.sort_values('ID')
+
+    # Check if there is any subject for which we have the fmri data but no demographics
+    missingsubjectsId = list(set(demographics['ID']) ^ set(rawsubjectsId))
+    # remove the demographic data from the missing subjects
+    demographics = demographics.loc[~demographics['ID'].isin(missingsubjectsId)]
+
+    return demographics
+
+
+def get_data(project_wd):
+    print('Getting data')
+    project_data = os.path.join(project_wd, 'data')
+    project_sink = os.path.join(project_wd, 'output')
+
+    # remove the file end and get list of all used subjects
+    fileList = os.listdir(project_data)
+    rawsubjectsId = [re.sub(r'^smwc1(.*?)\_mpr-1_anon.nii$', '\\1', file) for file in fileList if file.endswith('.nii')]
+    # TODO: Change this. For testing purpose select just the first 5 subjects
+    rawsubjectsId = rawsubjectsId[:25]
+
+    # Load the demographics for each subject
+    demographics = get_data_covariates(project_data, rawsubjectsId)
+
+    # Load image proxies
+    imgs = [nib.load(os.path.join(project_data, 'smwc1%s_mpr-1_anon.nii' %subject)) for subject in rawsubjectsId]
+
+    # Use nilearn to mask only the brain voxels across subjects
+    print('Compute brain mask')
+    MeanImgMask = masking.compute_multi_epi_mask(imgs, lower_cutoff=0.001, upper_cutoff=.7, opening=1)
+    # Apply the group mask on all subjects.
+    # Note: The apply_mask function returns the flattened data as a numpy array
+    maskedData = [masking.apply_mask(img, MeanImgMask) for img in imgs]
+    print('Applied mask to the dataset')
+    # Transform the imaging data into a dataframe (subjects x voxels)
+    imgDf = pd.DataFrame(data=maskedData)
+    return project_sink, demographics, imgs, maskedData, imgDf
--- BayOptPy/sklearn/regression.py
+++ BayOptPy/sklearn/regression.py
@@ -1,30 +1,12 @@
 #!/usr/bin/env python
 
 import os
-import re
-import pandas as pd
 import argparse
-
-from nilearn import masking
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.linear_model import LinearRegression
-import nibabel as nib
 
-
-def get_data_covariates(dataPath, rawsubjectsId):
-    # Load the demographic details from the dataset
-    demographics = pd.read_csv(os.path.join(dataPath, 'oasis_cross-sectional.csv'))
-    # sort demographics by ascending id
-    demographics = demographics.sort_values('ID')
-
-    # Check if there is any subject for which we have the fmri data but no demographics
-    missingsubjectsId = list(set(demographics['ID']) ^ set(rawsubjectsId))
-    # remove the demographic data from the missing subjects
-    demographics = demographics.loc[~demographics['ID'].isin(missingsubjectsId)]
-
-    return demographics
+from BayOptPy.helperfunctions import get_data
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-nogui',
@@ -35,35 +17,13 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
 
-    project_wd = '/BayOpt'
-    project_data = os.path.join(project_wd, 'data')
-    project_sink = os.path.join(project_wd, 'output')
-    print('Running regression analyis')
     print('The current args are: %s' %args)
+    #project_wd = '/BayOpt'
+    project_wd = os.getcwd()
+    project_sink, demographics, imgs, maskedData, imgDf = get_data(project_wd)
 
-    # Get nii images and list of subjects
-    dataPath = os.path.join(project_data)
-    # remove the file end and get list of all used subjects
-    fileList = os.listdir(dataPath)
-    rawsubjectsId = [re.sub(r'^smwc1(.*?)\_mpr-1_anon.nii$', '\\1', file) for file in fileList if file.endswith('.nii')]
-    # TODO: Change this. For testing purpose select just the first 5 subjects
-    rawsubjectsId = rawsubjectsId[:25]
+    print('Running regression analyis with sklearn')
 
-    # Load the demographics for each subject
-    demographics = get_data_covariates(dataPath, rawsubjectsId)
-
-    # Load image proxies
-    imgs = [nib.load(os.path.join(dataPath, 'smwc1%s_mpr-1_anon.nii' %subject)) for subject in rawsubjectsId]
-
-    # Use nilearn to mask only the brain voxels across subjects
-    print('Compute brain mask')
-    MeanImgMask = masking.compute_multi_epi_mask(imgs, lower_cutoff=0.001, upper_cutoff=.7, opening=1)
-    # Apply the group mask on all subjects.
-    # Note: The apply_mask function returns the flattened data as a numpy array
-    maskedData = [masking.apply_mask(img, MeanImgMask) for img in imgs]
-    print('Applied mask to the dataset')
-    # Transform the imaging data into a dataframe (subjects x voxels)
-    imgDf = pd.DataFrame(data=maskedData)
 
     # split train-test dataset
     targetAttribute = demographics['Age']
