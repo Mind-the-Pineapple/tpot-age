8ca633af394d1d7637d5efe4f2c0e47d83756804
Jessica Dafflon <jejedafflon@gmail.com>
2018-10-10 10:43:03 +0100

    Simple TPOT analysis
    * Analysis starts but does not conclude

---
+++ BayOptPy/tpot/simple_analysis.py
@@ -0,0 +1,41 @@
+import os
+import argparse
+from tpot import TPOTRegressor
+from sklearn import model_selection
+import numpy as np
+
+from BayOptPy.helperfunctions import get_data
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-nogui',
+                    dest='nogui',
+                    action='store_true',
+                    help='No gui')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    print('The current args are: %s' %args)
+
+    #project_wd = '/BayOpt'
+    project_wd = os.getcwd()
+    project_sink, demographics, imgs, maskedData, imgDf = get_data(project_wd)
+    # transform data into numpy array
+    data = np.array(maskedData)[:, :2000]
+    print('Running regression analyis with TPOT')
+    # split train-test dataset
+    targetAttribute = np.array(demographics['Age'])
+
+    Xtrain, Xtest, Ytrain, Ytest = model_selection.train_test_split(data, targetAttribute, test_size=.4, random_state=42)
+    print('Divided dataset into test and training')
+    print('Check train test split sizes')
+    print('X_train: ' + str(Xtrain.shape))
+    print('X_test: '  + str(Xtest.shape))
+    print('Y_train: ' + str(Ytrain.shape))
+    print('Y_test: '  + str(Ytest.shape))
+
+    tpot = TPOTRegressor(generations=5, population_size=20, n_jobs=-1, verbosity=2, max_time_mins=20, random_state=42,
+                         memory='auto')
+    # njobs=-1 uses all cores present in the machine
+    tpot.fit(Xtrain, Ytrain)
+    print(tpot.score(Xtest, Ytest))
+    print('Done TPOT analysis!')
