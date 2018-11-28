a4574d5b4e2e6dccec8a80026ce146bf52d101f8
Jessica Dafflon <jejedafflon@gmail.com>
2018-11-28 17:02:04 +0000

    Freesurfer analysis
    * Also check the right hemisphere

--- BayOptPy/check_freesurfer_stasts.py
+++ BayOptPy/check_freesurfer_stasts.py
@@ -5,19 +5,40 @@ import numpy as np
 from BayOptPy.helperfunctions import get_data_covariates, get_paths
 
 # Load the freesurfer aparc stats, remove the file endings and analyse the number of subjects
-print('Analysing and cleaning the aparc file')
-aparc_data = pd.read_csv('data/fsl/aparc_stats_lh.txt', sep='\t')
-aparc_data['lh.aparc.thickness'] = aparc_data['lh.aparc.thickness'].str.replace(r'_*.nii', '')
+print('Analysing and cleaning the aparc - LH file')
+aparc_data_lh = pd.read_csv('data/freesurfer_BANC2016/aparc_stats_lh.txt', sep='\t')
+aparc_data_lh['lh.aparc.thickness'] = aparc_data_lh['lh.aparc.thickness'].str.replace(r'_*.nii', '')
 # get rid of the T1
-aparc_data['lh.aparc.thickness'] = aparc_data['lh.aparc.thickness'].str.replace(r'_*.T1', '')
+aparc_data_lh['lh.aparc.thickness'] = aparc_data_lh['lh.aparc.thickness'].str.replace(r'_*.T1', '')
 # split the BANC id from its original dataset ID
-aparc_data['BANC_ID'] = aparc_data['lh.aparc.thickness'].str.split('_').str[0]
-aparc_data['orig_dataset_ID'] = aparc_data['lh.aparc.thickness'].str.split('_').str[1]
-print('aparc shape: (%d, %d)' %(aparc_data.shape[0], aparc_data.shape[1]))
+aparc_data_lh['BANC_ID'] = aparc_data_lh['lh.aparc.thickness'].str.split('_').str[0]
+aparc_data_lh['orig_dataset_ID'] = aparc_data_lh['lh.aparc.thickness'].str.split('_').str[1]
+print('aparc shape: (%d, %d)' %(aparc_data_lh.shape[0], aparc_data_lh.shape[1]))
+print(' ')
+
+print('Analysing and cleaning the aparc - RH file')
+aparc_data_rh = pd.read_csv('data/freesurfer_BANC2016/aparc_stats_rh.txt', sep='\t')
+aparc_data_rh['rh.aparc.area'] = aparc_data_rh['rh.aparc.area'].str.replace(r'_*.nii', '')
+# get rid of the T1
+aparc_data_rh['rh.aparc.area'] = aparc_data_rh['rh.aparc.area'].str.replace(r'_*.T1', '')
+# split the BANC id from its original dataset ID
+aparc_data_rh['BANC_ID'] = aparc_data_rh['rh.aparc.area'].str.split('_').str[0]
+aparc_data_rh['orig_dataset_ID'] = aparc_data_rh['rh.aparc.area'].str.split('_').str[1]
+print('aparc shape: (%d, %d)' %(aparc_data_rh.shape[0], aparc_data_rh.shape[1]))
+
+# Check if the LH and the RH have the same subjects
+if aparc_data_rh.shape[0] == aparc_data_lh.shape[0]:
+    print('Both hemispheres have the same number of subjects')
+else:
+    raise ValueError('The LH and RH hemisphere have different number of subjects')
+if np.sum(aparc_data_rh['rh.aparc.area'] == aparc_data_lh['lh.aparc.thickness']) == aparc_data_rh.shape[0]:
+    print('Both LH and RH are analysing the same subjects')
+print(' ')
+
 
 # Load the freesurfer aseg stats and do the same analysis done for the aparc file
 print('Analysing and cleaning the aseg file')
-aseg_data = pd.read_csv('data/fsl/aseg_stats.txt', sep='\t')
+aseg_data = pd.read_csv('data/freesurfer_BANC2016/aseg_stats.txt', sep='\t')
 # Process it in the same way as the aparc dataset
 aseg_data['Measure:volume'] = aseg_data['Measure:volume'].str.replace(r'_*.nii', '')
 # get rid of the T1
@@ -25,7 +46,8 @@ aseg_data['Measure:volume'] = aseg_data['Measure:volume'].str.replace(r'_*.T1', 
 print('aseg shape: (%d, %d)' %(aseg_data.shape[0], aseg_data.shape[1]))
 
 print('List of subjects present on the aparc but not on the aseg stats')
-print(list(set(aparc_data['lh.aparc.thickness']) - set(aseg_data['Measure:volume'])))
+print(list(set(aparc_data_lh['lh.aparc.thickness']) - set(aseg_data['Measure:volume'])))
+print(' ')
 
 # get the list of subjects and demographics for the BANC dataset
 debug = True
@@ -38,8 +60,8 @@ rawsubjectsId = [file[5:12] for file in fileList if file.endswith('.nii.gz')]
 demographics, selectedSubId = get_data_covariates(project_data, rawsubjectsId, dataset)
 
 # Compare the list of subjects from the BANC and the free-surfer analyis
-missa = np.sort(list(set(selectedSubId) - set(aparc_data['BANC_ID'].values)))
-missb = np.sort(list(set(aparc_data['BANC_ID'].values) - set(selectedSubId)))
+missa = np.sort(list(set(selectedSubId) - set(aparc_data_lh['BANC_ID'].values)))
+missb = np.sort(list(set(aparc_data_lh['BANC_ID'].values) - set(selectedSubId)))
 print('Number of missing subjects in the BANC dataset: %d' %len(missa))
 print('Number of missing subjects in the freesurfer dataset: %d' %len(missb))
 # number of subjects from the CanCan list
