dafe4e9512ab33a196fe62ed99c99b9a6fc46e83
Jessica Dafflon <jejedafflon@gmail.com>
2018-12-03 19:38:51 +0000

    Use aparc and aseg info to train regression models
    * Pass which dictionary to be used by TPOT as an argument to the function
    * Use the aparc and aseg information with the cortex thickness to regress the models
    * Use the ExtendedTPOTRegressor object to perform analysis
    * Remove NaNs and save the correlation matrix

--- BayOptPy/tpot/brain_age_analysis.py
+++ BayOptPy/tpot/brain_age_analysis.py
@@ -1,13 +1,16 @@
 import multiprocessing
 import os
 import argparse
-from tpot import TPOTRegressor
 from sklearn import model_selection
 import numpy as np
-import pickle
 #from dask.distributed import Client
+import seaborn as sns
+from matplotlib.pylab import plt
 
+from BayOptPy.tpot.extended_tpot import ExtendedTPOTRegressor
 from BayOptPy.helperfunctions import get_data, get_paths, get_config_dictionary
+from BayOptPy.tpot.custom_tpot_config_dict import tpot_config_custom
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-nogui',
@@ -28,7 +31,7 @@ parser.add_argument('-dask',
 parser.add_argument('-dataset',
                     dest='dataset',
                     help='Specify which dataset to use',
-                    choices=['OASIS', 'BANC']
+                    choices=['OASIS', 'BANC', 'BANC_freesurf']
                     )
 parser.add_argument('-cv',
                     dest='cv',
@@ -54,11 +57,10 @@ parser.add_argument('-resamplefactor',
                     type=int,
                     default=1 # no resampling is performed
                     )
-parser.add_argument('-nopreprocessing',
-                    dest='nopreprocessing',
-                    action='store_true',
-                    help='Use default TPOT light models without the preprocessing',
-                    default=False
+parser.add_argument('-config_dict',
+                    dest='config_dict',
+                    help='Specify which TPOT config dict to use',
+                    choices=['None', 'light', 'custom', 'ligth_no_preproc']
                     )
 
 args = parser.parse_args()
@@ -67,8 +69,21 @@ if __name__ == '__main__':
 
     print('The current args are: %s' %args)
 
+    # check which TPOT dictionary containing the operators and parameters to be used was passed as argument
+    if args.config_dict == 'None':
+        tpot_config = None
+    elif args.config_dict == 'light':
+        tpot_config = 'TPOT light'
+    elif args.config_dict == 'custom':
+        tpot_config = tpot_config_custom
+    elif args.config_dict == 'light_no_preproc':
+        # this option uses the TPOT light definition without the preprocessing
+        tpot_config = get_config_dictionary()
+
+    random_seed = 42
+
     project_wd, project_data, project_sink = get_paths(args.debug, args.dataset)
-    demographics, imgs, maskedData = get_data(project_data, args.dataset, args.debug, project_wd, args.resamplefactor)
+    demographics, imgs, data = get_data(project_data, args.dataset, args.debug, project_wd, args.resamplefactor)
 
     print('Running regression analyis with TPOT')
     # split train-test dataset
@@ -83,7 +98,8 @@ if __name__ == '__main__':
         client
 
     # To ensure the example runs quickly, we'll make the training dataset relatively small
-    Xtrain, Xtest, Ytrain, Ytest = model_selection.train_test_split(maskedData, targetAttribute, test_size=.5, random_state=42)
+    Xtrain, Xtest, Ytrain, Ytest = model_selection.train_test_split(data, targetAttribute, test_size=.25,
+                                                                    random_state=random_seed)
     print('Divided dataset into test and training')
     print('Check train test split sizes')
     print('X_train: ' + str(Xtrain.shape))
@@ -91,33 +107,42 @@ if __name__ == '__main__':
     print('Y_train: ' + str(Ytrain.shape))
     print('Y_test: '  + str(Ytest.shape))
 
-    # if no preprocessing is passed as argument do not perform any preprocessing on the pipelines
-    if args.nopreprocessing:
-        config_dic = get_config_dictionary()
-    else:
-        config_dic = 'TPOT light'
-
-    tpot = TPOTRegressor(generations=args.generations,
+    tpot = ExtendedTPOTRegressor(generations=args.generations,
                          population_size=args.population_size,
                          n_jobs=1,
                          cv=args.cv,
                          verbosity=2,
-                         # max_time_mins=20,
-                         random_state=42,
-                         config_dict=config_dic,
+                         random_state=random_seed,
+                         config_dict=tpot_config,
                          scoring='neg_mean_absolute_error',
                          use_dask=args.dask
-                         # memory='auto'
                         )
     print('Number of cross-validation: %d' %args.cv)
     print('Number of generations: %d' %args.generations)
     print('Population Size: %d' %args.population_size)
     # njobs=-1 uses all cores present in the machine
-    tpot.fit(Xtrain, Ytrain)
-    print(tpot.score(Xtest, Ytest))
+    tpot.fit(Xtrain, Ytrain, Xtest)
+    print('Test score using optimal model: %f ' % tpot.score(Xtest, Ytest))
     tpot.export('tpot_simple_analysis_pipeline.py')
     print('Done TPOT analysis!')
 
-    # Pickle dictionary with the the list of evaluated pipelines
-    with open(os.path.join(project_wd, 'BayOptPy', 'tpot', '%s_pipelines.pkl' %args.dataset), 'wb') as handle:
-        pickle.dump(tpot.evaluated_individuals_, handle)
+    # Do some preprocessing to find models where all predictions have the same value and eliminate them, as those will correspond
+    # to NaN entries or very small numbers on the correlation matrix.
+    repeated_idx = np.argwhere(
+        [np.array_equal(np.repeat(tpot.predictions[i][0], len(tpot.predictions[i])), tpot.predictions[i]) for i in
+         range(len(tpot.predictions))])
+    print('Index of the models with the same prediction for all subjects: ' + str(np.squeeze(repeated_idx)))
+    print('Number of models analysed: %d' % len(tpot.predictions))
+    tpot_predictions = np.delete(np.array(tpot.predictions), np.squeeze(repeated_idx), axis=0)
+    print('Number of models that will be used for cross-correlation: %s' % (tpot_predictions.shape,))
+
+    # Cross correlate the predictions
+    corr_matrix = np.corrcoef(tpot_predictions)
+
+    print('Check the number of NaNs after deleting models with constant predictions: %d' % len(
+        np.argwhere(np.isnan(corr_matrix))))
+    # colormap = sns.diverging_palette(220, 10, as_cmap=True)
+    sns.heatmap(corr_matrix, cmap='coolwarm')
+    plt.title(args.config_dict)
+    plt.savefig(os.path.join(project_wd, 'BayOptPy', 'tpot', 'cross_corr_%s.png' % args.config_dict))
+
