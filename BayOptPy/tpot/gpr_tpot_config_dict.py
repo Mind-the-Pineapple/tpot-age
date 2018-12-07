d65b5b40e84480e1e55325558e28055a530fa186
Jessica Dafflon <jejedafflon@gmail.com>
2018-12-07 15:57:34 +0000

    Add 2 possible config dictionaries with GPR
    * gpr_tpot_config_dict: Only use GPR with different kernels
    * gpr_tpot_config_dict_full: Use GPR with different kernels and other models

--- BayOptPy/tpot/gpr_tpot_config_dict.py
+++ BayOptPy/tpot/gpr_tpot_config_dict.py
@@ -8,28 +8,21 @@ from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
                                               ExpSineSquared, DotProduct,
                                               ConstantKernel)
 
-# kernels = [RBF(length_scale=x, length_scale_bounds=(y, z)) for x, y, z in
-#                    zip([1.],
-#                        [1e-01],
-#                        [10.])]
-
 kernels_rbf = [RBF(length_scale=x) for x in np.arange(0., 1.1, .1)]
-kernels_rq = [RationalQuadratic(length_scale=x) for x in np.arange(0., 1.1, .1)]
-kernels_exp = [ExpSineSquared(length_scale=x) for x in np.arange(0., 1.1, .1)]
+kernels_rq = [RationalQuadratic(length_scale=x, alpha=y) for x,y in
+              product(np.arange(0., 1.1, .1), np.arange(0.1, 10.1,1))]
+kernels_exp = [ExpSineSquared(length_scale=x, periodicity=y) for x,y in
+                     product(np.arange(0., 1.1, .1), np.arange(.01, 10.1,1))]
 kernels_mat = [Matern(length_scale=x, nu=y) for x,y in
-                                product(np.arange(0.1, 1.1,.1), [.5, 1.5, 2.5])]
-kernel_dot = [DotProduct(sigma_0=x) for x in np.arange(0., 1, .1)]
+                     product(np.arange(0.1, 1.1,.1), [.5, 1.5, 2.5])]
+kernel_dot = [DotProduct(sigma_0=x) for x in np.arange(0., 1, .01)]
 kernels = kernels_rbf + kernels_rq + kernels_exp + kernels_mat + kernel_dot
 
 
-# kernels = [1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0)),
-#            1.0 * RBF(length_scale=.8, length_scale_bounds=(1e-2, 100.0)),
-#            1.0 * RationalQuadratic(length_scale=1.0, alpha=0.1),
-#            1.0 * ExpSineSquared(length_scale=1.0, periodicity=3.0,
-#                 length_scale_bounds=(0.1, 10.0), periodicity_bounds=(1.0, 10.0)),
-#           ]
 tpot_config_gpr = {
     'sklearn.gaussian_process.GaussianProcessRegressor': {
         'kernel': kernels,
+        'random_state': [42],
+        'alpha': [1e-10, 1e-8, 1e-5, 1e-3, 1e-2, 1e0]
     },
 }
