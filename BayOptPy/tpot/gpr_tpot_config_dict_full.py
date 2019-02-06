174929bccd00ea5c41f852df5cc2dfbb0dea3eed
Jessica Dafflon <jejedafflon@gmail.com>
2019-02-06 14:45:18 +0000

    Modify TPOT config dictionary
    * Add Nystroem and RBF Sample to the possible preprocessors
    * Add Gradient BoostingRegressor and DecisionTreeRegressor

--- BayOptPy/tpot/gpr_tpot_config_dict_full.py
+++ BayOptPy/tpot/gpr_tpot_config_dict_full.py
@@ -53,6 +53,15 @@ tpot_config_gpr = {
                  10., 15., 20., 25.],
            'epsilon': [1e-4, 1e-3, 1e-2, 1e-1, 1.]
     },
+    # 'sklearn.svm.SVR':{
+    #        'kernel': ["rbf", "sigmoid"],
+    #        'degree': np.arange(1, 5, 1),
+    #        'coef0':  np.arange(0, 1, .1),
+    #        'C': [1e-4, 1e-3, 1e-2, 1e-1, 0.5, 1., 5.,
+    #              10., 15., 20., 25.],
+    #        'epsilon': [1e-4, 1e-3, 1e-2, 1e-1, 1.],
+    #        'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+    # },
     'sklearn.ensemble.RandomForestRegressor': {
            'n_estimators': [100],
            'max_features': np.arange(0.05, 1.01, 0.05),
@@ -61,5 +70,33 @@ tpot_config_gpr = {
            'bootstrap': [True, False]
     },
     'sklearn.linear_model.RidgeCV': {
-    }
+    },
+    'sklearn.ensemble.GradientBoostingRegressor': {
+           'n_estimators': [100],
+           'loss': ["ls", "lad",  "huber", "quantile"],
+           'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
+           'max_depth': range(1,11),
+           'min_samples_split': range(2, 21),
+           'min_samples_leaf': range(1, 21),
+           'subsample': np.arange(0.05, 1.01, 0.05),
+           'max_features': np.arange(0.05, 1.01, 0.05),
+           'alpha': [0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
+    },
+        'sklearn.tree.DecisionTreeRegressor': {
+           'max_depth': range(1, 11),
+           'min_samples_split': range(2, 21),
+           'min_samples_leaf': range(1, 21)
+    },
+###############################################################################
+# Preprocessors
+###############################################################################
+    'sklearn.kernel_approximation.Nystroem': {
+            'kernel': ['rbf', 'cosine', 'chi2', 'laplacian', 'polynomial', 'poly',
+                       'linear', 'additive_chi2', 'sigmoid'],
+            'gamma': np.arange(0.0, 1.01, 0.05),
+            'n_components': range(1, 11)
+    },
+        'sklearn.kernel_approximation.RBFSampler': {
+            'gamma': np.arange(0.0, 1.01, 0.05)
+    },
 }
