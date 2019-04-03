7d9687fb9c0056ec093daf83cd82fdb080b74ab3
Jessica Dafflon <jejedafflon@gmail.com>
2019-04-03 12:59:32 +0100

    Add forgotten TPOT dictionary and Freesurfer analysis
    * TPOT:
        * Add forgotten configuration dictionary
    * Freesurfer:
        * Select only freesurfer features that are present on the BANC dataset.
        * Boxplot from the lhCerebralWhiteMatterVol, rhCerebralWhiteMatterVol, CerebralWhiteMatterVol for both the ukbiobanc and the banc dataset
        * Calculate the mean thickness for the left and right hemisphere for both datasets.
        * Plot the difference in mean (error mean) between both hemisphere for the BANC dataset

---
+++ BayOptPy/tpot/gpr_tpot_config_vanilla.py
@@ -0,0 +1,116 @@
+# Custom defined list of Gaussian Process regression models to be used by TPOT
+import numpy as np
+import pdb
+from itertools import product
+
+from skrvm import RVR
+
+# Define list of Kernels
+from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
+                                              ExpSineSquared, DotProduct,
+                                              ConstantKernel)
+kernels_rbf = [RBF(length_scale=x) for x in np.arange(0., 1.1, .1)]
+kernels_rq = [RationalQuadratic(length_scale=x, alpha=y) for x,y in
+              product(np.arange(0., 1.1, .1), np.arange(0.1, 10.1,1))]
+kernels_exp = [ExpSineSquared(length_scale=x, periodicity=y) for x,y in
+                     product(np.arange(0., 1.1, .1), np.arange(.01, 10.1,1))]
+kernels_mat = [Matern(length_scale=x, nu=y) for x,y in
+                     product(np.arange(0.1, 1.1,.1), [.5, 1.5, 2.5])]
+kernel_dot = [DotProduct(sigma_0=x) for x in np.arange(0., 1, .01)]
+kernels = kernels_rbf + kernels_rq + kernels_exp + kernels_mat + kernel_dot
+
+tpot_config_gpr = {
+    'sklearn.gaussian_process.GaussianProcessRegressor': {
+        'kernel': kernels,
+        'random_state': [42],
+        'alpha': [1e-100, 1e-20, 1e-12, 1e-10, 1e-5, 1e-3]
+    },
+    'skrvm.RVR': {
+           'kernel': kernels,
+           'alpha': [1e-10, 1e-06, 1e-02, 1],
+           'beta': [1e-10, 1e-06, 1e-02, 1],
+
+                 },
+    'sklearn.svm.LinearSVR': {
+           'loss': ["epsilon_insensitive",
+                    "squared_epsilon_insensitive"],
+           'dual': [True, False],
+           'random_state': [42],
+           'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
+           'C': [1e-4, 1e-3, 1e-2, 1e-1, 0.5, 1., 5.,
+                 10., 15., 20., 25.],
+           'epsilon': [1e-4, 1e-3, 1e-2, 1e-1, 1.]
+    },
+    'sklearn.ensemble.RandomForestRegressor': {
+           'n_estimators': [100],
+           'max_features': np.arange(0.05, 1.01, 0.05),
+           'random_state': [42],
+           'min_samples_split': range(2, 21),
+           'min_samples_leaf':  range(1, 21),
+           'bootstrap': [True, False]
+    },
+    'sklearn.neighbors.KNeighborsRegressor': {
+         'n_neighbors': range(1,101),
+          'weights': ["uniform", "distance"],
+          'p': [1, 2]
+    },
+    'sklearn.linear_model.LinearRegression':{
+        },
+    'sklearn.linear_model.Ridge': {
+        'alpha': [1.0, 10.0, 100.0],
+        'random_state': [42],
+        },
+    # Additional models
+    'sklearn.linear_model.ElasticNetCV': {
+        'l1_ratio': np.arange(0.0, 1.01, 0.05),
+        'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
+        'random_state': [42],
+    },
+    'sklearn.ensemble.ExtraTreesRegressor': {
+        'n_estimators': [100],
+        'max_features': np.arange(0.05, 1.01, 0.05),
+        'min_samples_split': range(2, 21),
+        'min_samples_leaf': range(1, 21),
+        'random_state': [42],
+        'bootstrap': [True, False]
+    },
+    'sklearn.neighbors.KNeighborsRegressor': {
+         'n_neighbors': range(1,101),
+          'weights': ["uniform", "distance"],
+          'p': [1, 2]
+    },
+    'sklearn.linear_model.LassoLarsCV': {
+           'normalize': [True, False]
+    },
+        'sklearn.tree.DecisionTreeRegressor': {
+           'max_depth': range(1, 11),
+           'min_samples_split': range(2, 21),
+           'random_state': [42],
+           'min_samples_leaf': range(1, 21)
+    },
+###############################################################################
+## Preprocessors
+################################################################################
+    'sklearn.kernel_approximation.Nystroem': {
+            'kernel': ['rbf', 'cosine', 'chi2', 'laplacian', 'polynomial', 'poly',
+                       'linear', 'additive_chi2', 'sigmoid'],
+            'gamma': np.arange(0.0, 1.01, 0.05),
+            'random_state': [42],
+            'n_components': range(1, 11)
+    },
+        'sklearn.preprocessing.RobustScaler': {
+                                              },
+         'sklearn.preprocessing.StandardScaler': {
+                                                 },
+        'sklearn.kernel_approximation.RBFSampler': {
+            'gamma': np.arange(0.0, 1.01, 0.05),
+           'random_state': [42],
+    },
+        'sklearn.preprocessing.MaxAbsScaler': {
+                                              },
+        'sklearn.preprocessing.MinMaxScaler': {
+                                              },
+        'sklearn.preprocessing.Normalizer':{
+            'norm':['l1', 'l2', 'max']
+    },
+}
--- UKBIO/general_analysis.py
+++ UKBIO/general_analysis.py
@@ -3,6 +3,9 @@
 import pandas as pd
 import numpy as np
 import pdb
+import seaborn as sns
+sns.set()
+import matplotlib.pyplot as plt
 
 from BayOptPy.helperfunctions import get_paths, get_data
 
@@ -21,6 +24,17 @@ demographics_ukbio, imgs_ukbio, data_ukbio, freesurfer_df_ukbio = get_data(proje
 # First print the size of dataset
 print(freesurfer_df_banc.shape, freesurfer_df_ukbio.shape)
 
+# Save the lh_MeanThickness and rh_MeanThickness to compare it to calculated
+# value afterwards
+lh_MeanThickness_banc = freesurfer_df_banc['lh_MeanThickness_thickness']
+rh_MeanThickness_banc = freesurfer_df_banc['rh_MeanThickness_thickness']
+
+# remove the columns from the banc dataset that are not present in the BIOBANK
+missing_columns = ['lh_MeanThickness_thickness', 'BrainSegVolNotVent',
+                   'eTIV', 'rh_MeanThickness_thickness', 'BrainSegVolNotVent.1',
+                  'eTIV.1']
+freesurfer_df_banc = freesurfer_df_banc.drop(missing_columns, axis=1)
+
 # Save the colunns as varaible
 freesurfer_banc_columns = list(freesurfer_df_banc)
 freesurfer_ukbio_columns = list(freesurfer_df_ukbio)
@@ -30,7 +44,157 @@ print('Columns that are missing in the ukbiobanc but are on the banc dataset')
 print(set(freesurfer_banc_columns).difference(freesurfer_ukbio_columns))
 print('Columns that are missing in the bancdataset but are on the ukbiobanc')
 print(set(freesurfer_ukbio_columns).difference(freesurfer_banc_columns))
-print('Columns that are ')
+
+df_ukbio_thickness = freesurfer_df_ukbio.loc[:,
+                            freesurfer_df_ukbio.columns.str.contains('thk')]
+
+# Create a dictionary matching the columns from the ukbiobank to the BANC
+# dataset and rename ukbiobank columns.
+lh_thickness = [
+                   # left hemisphere
+                  'lh_thk_bankssts', 'lh_thk_caudalanteriorcingulate',
+                  'lh_thk_caudalmiddlefrontal', 'lh_thk_cuneus',
+                  'lh_thk_entorhinal', 'lh_thk_fusiform',
+                  'lh_thk_inferiorparietal', 'lh_thk_inferiortemporal',
+                  'lh_thk_isthmus', 'lh_thk_lateraloccipital',
+                  'lh_thk_lateralorbitofrontal', 'lh_thk_lingual',
+                  'lh_thk_medialorbitofrontal',
+                  'lh_thk_middletemporal', 'lh_thk_parahippocampal',
+                  'lh_thk_paracentral', 'lh_thk_parsopercularis',
+                  'lh_thk_parsorbitalis', 'lh_thk_parstriangularis',
+                  'lh_thk_pericalcarine', 'lh_thk_postcentral',
+                  'lh_thk_posteriorcingulate', 'lh_thk_precentral',
+                  'lh_thk_precuneus', 'lh_thk_rostralanteriorcingulate',
+                  'lh_thk_rostralmiddlefrontal', 'lh_thk_superiorfrontal',
+                  'lh_thk_superiorparietal', 'lh_thk_superiortemporal',
+                  'lh_thk_supramarginal', 'lh_thk_frontalpole',
+                  'lh_thk_temporalpole', 'lh_thk_transversetemporal',
+                  'lh_thk_insula',
+               ]
+
+rh_thickness = [
+                   # right hemisphere
+                  'rh_thk_bankssts', 'rh_thk_caudalanteriorcingulate',
+                  'rh_thk_caudalmiddlefrontal', 'rh_thk_cuneus',
+                  'rh_thk_entorhinal', 'rh_thk_fusiform',
+                  'rh_thk_inferiorparietal', 'rh_thk_inferiortemporal',
+                  'rh_thk_isthmus', 'rh_thk_lateraloccipital',
+                  'rh_thk_lateralorbitofrontal', 'rh_thk_lingual',
+                  'rh_thk_medialorbitofrontal', 'rh_thk_middletemporal',
+                  'rh_thk_parahippocampal', 'rh_thk_paracentral',
+                  'rh_thk_parsopercularis', 'rh_thk_parsorbitalis',
+                  'rh_thk_parstriangularis', 'rh_thk_pericalcarine',
+                  'rh_thk_postcentral', 'rh_thk_posteriorcingulate',
+                  'rh_thk_precentral', 'rh_thk_precuneus',
+                  'rh_thk_rostralanteriorcingulate',
+                  'rh_thk_rostralmiddlefrontal', 'rh_thk_superiorfrontal',
+                  'rh_thk_superiorparietal', 'rh_thk_superiortemporal',
+                  'rh_thk_supramarginal', 'rh_thk_frontalpole',
+                  'rh_thk_temporalpole', 'rh_thk_transversetemporal',
+                  'rh_thk_insula',
+               ]
+
+biobank_columns =  lh_thickness + rh_thickness +  [
+                  # Additional features
+                   'Left.Lateral.Ventricle', 'Left.Inf.Lat.Vent',
+                  'Left.Cerebellum.White.Matter', 'Left.Cerebellum.Cortex',
+                  'Left.Thalamus.Proper', 'Left.Caudate', 'Left.Putamen',
+                  'Left.Pallidum', 'X3rd.Ventricle', 'X4th.Ventricle',
+                  'Brain.Stem', 'Left.Hippocampus', 'Left.Amygdala', 'CSF',
+                  'Left.Accumbens.area', 'Left.VentralDC', 'Left.vessel',
+                  'Left.choroid.plexus',
+                  'Right.Lateral.Ventricle',
+                  'Right.Inf.Lat.Vent', 'Right.Cerebellum.White.Matter',
+                  'Right.Cerebellum.Cortex', 'Right.Thalamus.Proper',
+                  'Right.Caudate', 'Right.Putamen', 'Right.Pallidum',
+                  'Right.Hippocampus', 'Right.Amygdala', 'Right.Accumbens.area',
+                  'Right.VentralDC', 'Right.vessel', 'Right.choroid.plexus',
+                  'X5th.Ventricle', 'WM.hypointensities',
+                  'Left.WM.hypointensities', 'Right.WM.hypointensities',
+                  'non.WM.hypointensities', 'Left.non.WM.hypointensities',
+                  'Right.non.WM.hypointensities', 'Optic.Chiasm',
+                  # the same
+                  'CC_Posterior',
+                  'CC_Mid.Posterior', 'CC_Central', 'CC_Mid_Anterior',
+                  'CC_Anterior', 'BrainSegVol', 'BrainSegVolNotVent',
+                  'BrainSegVolNotVentSurf', 'lhCortexVol', 'rhCortexVol',
+                  'CortexVol',
+                  # Missing in the bionk we have
+                  'lhCorticalWhiteMatterVol', 'rhCorticalWhiteMatterVol',
+                  'CorticalWhiteMatterVol',
+                  # 'lhCerebralWhiteMatterVol',
+                  # 'rhCerebralWhiteMatterVol', 'CerebralWhiteMatterVol',
+                  'SubCortGrayVol', 'TotalGrayVol', 'SupraTentorialVol',
+                  'SupraTentorialVolNotVent', 'SupraTentorialVolNotVentVox',
+                  'MaskVol', 'BrainSegVol.to.eTIV', 'MaskVol.to.eTIV',
+                  'lhSurfaceHoles', 'rhSurfaceHoles', 'SurfaceHoles',
+                  'EstimatedTotalIntraCranialVol'
+                 ]
+# Check if you have all the features form the banc dataset
+assert(len(biobank_columns), len(freesurfer_banc_columns))
+
+renameCols = dict(zip(biobank_columns, freesurfer_banc_columns))
+freesurfer_df_ukbio.rename(columns=renameCols, inplace=True)
+
+# Keep only the columns that both datasets have in common
+df_ukbio = freesurfer_df_ukbio[freesurfer_banc_columns]
+variables_of_interest = ['lhCerebralWhiteMatterVol', 'rhCerebralWhiteMatterVol',
+                         'CerebralWhiteMatterVol']
+# Generate a few plots to make sure you are comparing the same things among both
+# datasets
+df_ukbio['dataset'] = 'ukbio'
+freesurfer_df_banc['dataset'] = 'banc'
+df_combined = pd.concat((df_ukbio, freesurfer_df_banc))
+
+for idx, variable in enumerate(variables_of_interest):
+    fig = plt.figure()
+    sns_boxplot = sns.boxplot(x='dataset', y=variable,
+                          data=df_combined)
+    plt.title(variable)
+    fig = sns_boxplot.get_figure()
+    fig.savefig('/code/UKBIO/boxplot_%s.png' %(variable))
+    plt.close()
+
+# Calculate the Mean Thickness for both the left and the right hemisphere and
+# plot the different between both datastes.
+# Get the corresponding list of labels for the BANC dataset
+banc_lh_thickness = [renameCols[x] for x in lh_thickness]
+banc_rh_thickness = [renameCols[x] for x in rh_thickness]
+
+df_combined['lh_MeanThickness_thickness'] = df_combined[banc_lh_thickness].mean(axis=1)
+df_combined['rh_MeanThickness_thickness'] = df_combined[banc_rh_thickness].mean(axis=1)
+
+# Check if the calculated mean value is the same as the given mean value for the
+# banc dataset
+calculated_lh_MeanThickness_banc = \
+    df_combined[df_combined['dataset'] == 'banc']['lh_MeanThickness_thickness']
+calculated_rh_MeanThickness_banc = \
+    df_combined[df_combined['dataset'] == 'banc']['rh_MeanThickness_thickness']
+fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, sharey=True)
+plt.subplot(2, 1, 1)
+plt.plot(range(len(lh_MeanThickness_banc)), calculated_lh_MeanThickness_banc -
+         lh_MeanThickness_banc, marker='o', color='b')
+plt.title('Left Hemisphere')
+plt.subplot(2, 1, 2)
+plt.plot(range(len(rh_MeanThickness_banc)), calculated_rh_MeanThickness_banc -
+         rh_MeanThickness_banc, marker='o', color='r')
+plt.title('Right Hemisphere')
+fig.text(0.5, 0.01, 'Subject ID', ha='center')
+fig.text(0.01, 0.5, 'calculated - true', va='center', rotation='vertical')
+plt.tight_layout() # correct for overlaying layout
+plt.savefig('/code/UKBIO/MeanThickness.png')
+plt.close()
+
+pdb.set_trace()
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+sns.boxplot(x='dataset', y='lh_MeanThickness_thickness',
+                          data=df_combined, ax=ax1)
+sns.boxplot(x='dataset', y='rh_MeanThickness_thickness',
+                          data=df_combined, ax=ax2)
+fig.savefig('/code/UKBIO/boxplot_MeanThickness.png')
+plt.close()
 
 print('I am done!')
 # Compare the columns entry
