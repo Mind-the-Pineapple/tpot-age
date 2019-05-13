1c0951330032de6e55845fde90eb75355ced3e87
Jessica Dafflon <jejedafflon@gmail.com>
2019-05-13 11:09:12 +0100

    Validate suggested model with cleaned TPOT on BIOBANK dataset

--- BayOptPy/freesurfer_preprocess/original_dataset/UKBIO/tpot_model_analysis_4595743.py
+++ BayOptPy/freesurfer_preprocess/original_dataset/UKBIO/tpot_model_analysis_4595743.py
@@ -6,11 +6,13 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
-from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import ElasticNetCV
 from tpot.builtins import StackingEstimator
 
 from BayOptPy.helperfunctions import (get_paths, get_data,
@@ -19,15 +21,16 @@ from BayOptPy.helperfunctions import (get_paths, get_data,
 
 """
 This script tests the best model recommened by TPOT for 100 generations, random
-seed 20, initial population 1000, mutation rate and cross-validation ratexxx
+seed 42, initial population 1000, mutation rate and cross-validation rate 0.9
+and cross-over 0.1
 """
 
 # General Settings
 #-------------------------------------------------------------------------------
 debug = False
 resamplefactor = 1
-random_seed = 20
-save_path = '/code/BayOptPy/'
+random_seed = 42
+save_path = '/code/BayOptPy/tpot/Output/random_seed/100_generations/random_seed_042/'
 # Load the clean data for both the UKBIO and the BANC analysis
 # This version of the UKBIOBANK dataset contains the same columns as the BANC
 # dataset
@@ -59,32 +62,51 @@ targetAttribute_ukbio = np.array(age_UKBIO['age'])
 data_banc = df_banc.values
 # Find age for the BANC dataset
 targetAttribute_banc = np.array(demographics_banc['Age'])
-Xtrain, Xtest, Ytrain, Ytest = train_test_split(data_banc, targetAttribute_banc,
+Xtrain_banc, Xtest_banc, Ytrain_banc, Ytest_banc = train_test_split(data_banc, targetAttribute_banc,
                                                 test_size=.25,
                                                 random_state=random_seed)
 print('Divided BANC dataset into test and training')
 print('Check train test split sizes')
-print('X_train: ' + str(Xtrain.shape))
-print('X_test: '  + str(Xtest.shape))
-print('Y_train: ' + str(Ytrain.shape))
-print('Y_test: '  + str(Ytest.shape))
+print('X_train: ' + str(Xtrain_banc.shape))
+print('X_test: '  + str(Xtest_banc.shape))
+print('Y_train: ' + str(Ytrain_banc.shape))
+print('Y_test: '  + str(Ytest_banc.shape))
 
 # Best pipeline recommended by TPOT
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=Ridge(alpha=10.0, random_state=42)),
-    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=True, max_features=0.7500000000000001, min_samples_leaf=4, min_samples_split=4, n_estimators=100, random_state=42)),
-    ExtraTreesRegressor(bootstrap=False, max_features=0.9000000000000001, min_samples_leaf=3, min_samples_split=2, n_estimators=100, random_state=42)
-)
+                           StackingEstimator(estimator=Ridge(alpha=100.0,
+                                                             random_state=42)),
+                           MinMaxScaler(),
+                           StackingEstimator(estimator=ElasticNetCV(l1_ratio=0.8,
+                                                                    random_state=42,
+                                                                    tol=0.1)),
+                           StackingEstimator(estimator=RandomForestRegressor(bootstrap=True,
+                                                                             max_features=0.45,
+                                                                             min_samples_leaf=5,
+                                                                             min_samples_split=15,
+                                                                             n_estimators=100,
+                                                                             random_state=42)),
+                           RandomForestRegressor(bootstrap=True,
+                                                 max_features=0.6500000000000001,
+                                                 min_samples_leaf=2,
+                                                 min_samples_split=12,
+                                                 n_estimators=100,
+                                                 random_state=42)
+                                 )
 
-exported_pipeline.fit(Xtrain, Ytrain)
-y_predicted_banc = exported_pipeline.predict(Xtest)
-mae_banc = mean_absolute_error(Ytest, y_predicted_banc)
-print('Print BANC MAE')
+exported_pipeline.fit(Xtrain_banc, Ytrain_banc)
+print('Print BANC MAE - test')
+y_predicted_banc = exported_pipeline.predict(Xtest_banc)
+mae_banc = mean_absolute_error(Ytest_banc, y_predicted_banc)
 print(mae_banc)
+print('Print BANC MAE - training')
+y_predicted_banc_train = exported_pipeline.predict(Xtrain_banc)
+mae_banc_train = mean_absolute_error(Ytrain_banc, y_predicted_banc_train)
+print(mae_banc_train)
 
 # plot predicted vs true for the BIOBANK
 fig = plt.figure()
-plt.scatter(Ytest, y_predicted_banc)
+plt.scatter(Ytest_banc, y_predicted_banc)
 plt.ylabel('Predicted Age')
 plt.xlabel('True Age')
 plt.savefig(os.path.join(save_path, 'banc_predicted_true_age.png'))
